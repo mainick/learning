d572eadaec969bdcc707d17763f2c99120d8b22f
mainick <maico.orazio@gmail.com>
2021-12-28 13:19:54 +0100

    React Query: initial and placeholder query data

--- src/hooks/useFetchExercise.js
+++ src/hooks/useFetchExercise.js
@@ -1,14 +1,36 @@
-import { useQuery } from 'react-query'
+import { useQuery, useQueryClient } from 'react-query'
 import { getExerciseById } from '../services/ExerciseApi'
 import exerciseKeys from '../utils/ReactQueryKeyFactories'
 
 const useFetchExercise = (exerciseId) => {
+  const queryClient = useQueryClient()
+
   const { isLoading, isError, data, error, isFetching } = useQuery(
     exerciseKeys.detail(exerciseId),
     async () => getExerciseById(exerciseId),
     {
+      staleTime: 60 * 1000, // 1 minute in cache
       retry: 3,
       useErrorBoundary: (errorResp) => errorResp.response?.status >= 500,
+      placeholderData: () => {
+        const stateQueryExercisesAll = queryClient.getQueryState(
+          exerciseKeys.list('all')
+        )
+        if (
+          stateQueryExercisesAll &&
+          Date.now() - stateQueryExercisesAll.dataUpdatedAt <= 30 * 1000
+        ) {
+          // cached for half a minute
+          const found = stateQueryExercisesAll.data.find(
+            (item) => item.id === exerciseId
+          )
+          return found ?? undefined
+        }
+        return undefined
+      },
+      initialDataUpdatedAt: () =>
+        queryClient.getQueryState(exerciseKeys.list('all'), { exact: true })
+          ?.dataUpdatedAt,
     }
   )
 
--- src/hooks/useFetchExercises.js
+++ src/hooks/useFetchExercises.js
@@ -1,14 +1,35 @@
-import { useQuery } from 'react-query'
+import { useQuery, useQueryClient } from 'react-query'
 import { getExercises } from '../services/ExerciseApi'
 import exerciseKeys from '../utils/ReactQueryKeyFactories'
 
 const useFetchExercises = (stateFilter) => {
+  const queryClient = useQueryClient()
+
   const { isLoading, isError, data, error, isFetching } = useQuery(
     exerciseKeys.list(stateFilter),
     async () => getExercises(stateFilter),
     {
+      staleTime: 60 * 1000, // 1 minute in cache
       retry: false,
       useErrorBoundary: true,
+      initialData: () => {
+        const stateQueryExercisesAll = queryClient.getQueryState(
+          exerciseKeys.list('all')
+        )
+        if (stateQueryExercisesAll) {
+          if (stateFilter !== 'all') {
+            const isComplete = stateFilter === 'completed'
+            const filteredExercises =
+              stateQueryExercisesAll.data.filter(
+                (item) => item.complete === isComplete
+              ) ?? []
+            return filteredExercises.length > 0 ? filteredExercises : undefined
+          }
+          return stateQueryExercisesAll.data ?? undefined
+        }
+        return undefined
+      },
+      initialDataUpdatedAt: Date.now(),
     }
   )
 
